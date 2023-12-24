4239f90a57b3299a75fa310bdaf17509c3b2c72a
imPDA <dimaxa7@mail.ru>
2023-12-24 07:10:12 +0300

    url builder improvement

--- src/shikimori_extended_api/utils/url_builder.py
+++ src/shikimori_extended_api/utils/url_builder.py
@@ -2,6 +2,10 @@ from typing import Any, Self
 from urllib.parse import urlencode
 
 
+def skip_none(kwargs: dict) -> dict:
+    return {k: v for k, v in kwargs.items() if v is not None}
+
+
 class URL:
     def __init__(self, url: str):
         self.url = url
@@ -17,12 +21,16 @@ class URL:
         # in all other cases just add a new section to url
         return self._add(item)
 
+    def __getitem__(self, arg):
+        """Alternative to id() and paste()"""
+        return self._add(arg)
+
     def __call__(self, arg: Any = None, /, **kwargs) -> Self | str:
         # if a positional arg provided, add to the url path
         if arg:
             return self._add(arg)
 
-        return f"{self.url}{'?' if kwargs else ''}{urlencode(kwargs)}"
+        return f"{self.url}{'?' if kwargs else ''}{urlencode(skip_none(kwargs))}"
 
     def __str__(self) -> str:
         return self.url
